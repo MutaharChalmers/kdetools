Add CV-based bandwidth selection and fixed numerical instability in conditional sampling

kdetools/kdecond.py
@@ -8,7 +8,7 @@ from scipy._lib._util import check_random_state
 
 class gaussian_kde(st.gaussian_kde):
     """Superclass of the `scipy.stats.gaussian_kde` class, adding
-    conditional sampling functionality."""
+    conditional sampling and bandwidth selection by cross-validation."""
  
     def __init__(self, dataset, bw_method=None):
         """Create superclass of scipy gaussian_kde.
@@ -41,7 +41,9 @@ class gaussian_kde(st.gaussian_kde):
         k = mu.shape[1]
 
         # Eigenvalues and eigenvectors of covariance matrix
-        s, u = np.linalg.eigh(cov)
+        s, u = np.linalg.eigh(np.atleast_2d(cov))
+        # Occasionally the smallest eigenvalue is negative
+        s = np.abs(s) + np.spacing(1)
 
         # Terms in the log-pdf
         klog_2pi = k*np.log(2*np.pi)
@@ -55,6 +57,43 @@ class gaussian_kde(st.gaussian_kde):
     def _mvn_pdf(self, x, mu, cov):
         """Vectorised evaluation of multivariate normal pdf for KDE."""
         return np.exp(self._mvn_logpdf(x, mu, cov))
+
+    def kfold_split(self, X, k):
+        """Lightweight k-fold CV function to avoid sklearn dependency."""
+        splits = np.array_split(np.arange(X.shape[0]), k)
+        return [(np.concatenate([splits[i] for i in set(range(k))-{j}]),
+                 splits[j]) for j in range(k)]
+
+    def set_bandwidth(self, bw_method=None, k=5, delta=1, n=101):
+        """Add bandwidth selection by cross-validation.
+
+        Parameters
+        ----------
+        bw_method : str, scalar or callable, optional
+            As parent class, with extra 'cv' option.
+        k : int, optional
+            Number of folds in cross-validation.
+        delta : float, optional
+            Defines range of bandwidth grid in log space.
+        n : int, optional
+            Defines number of points in bandwidth grid.
+        """
+
+        if bw_method == 'cv':
+            # Define trial bandwidths around initial guess of Silverman factor
+            factor = self.silverman_factor()
+            bws = np.exp(np.log(factor) + np.linspace(-delta, delta, n))
+
+            # Cross-validation on log-likelihoods
+            eps = np.spacing(1)
+            x = self.dataset.T
+            bw_lls = [np.mean([-np.log(self._mvn_pdf(x[j], x[i], bw**2*np.cov(x[i].T)
+                                                    ).mean(axis=1)+eps).sum()
+                               for i, j in self.kfold_split(x, k)])
+                      for bw in bws]
+            super(gaussian_kde, self).set_bandwidth(bw_method=bws[np.argmin(bw_lls)])
+        else:
+            super(gaussian_kde, self).set_bandwidth(bw_method=bw_method)
 
     def conditional_resample(self, size, x_cond, dims_cond, seed=None):
         """Fast conditional sampling of estimated pdf.
@@ -93,9 +132,12 @@ class gaussian_kde(st.gaussian_kde):
         B = self.covariance[np.ix_(dims_samp, dims_cond)]
         C = self.covariance[np.ix_(dims_cond, dims_cond)]
 
-        # Evaluate densities at x_cond for all kernels
-        densities = self._mvn_pdf(x_cond, self.dataset[dims_cond].T, C).T
-        ps = (densities/densities.sum(axis=0)).T
+        # Evaluate log-densities at x_cond for all kernels
+        logpdfs = self._mvn_logpdf(x_cond, self.dataset[dims_cond].T, C)
+
+        # Convert to probabilities by correcting for precision then normalising
+        pdfs = np.exp(logpdfs.T-logpdfs.max(axis=1))
+        ps = (pdfs/pdfs.sum(axis=0)).T
 
         # Sample dataset kernels proportional to normalised pdfs at x_cond
         counts = np.array([random_state.multinomial(size, p) for p in ps])
@@ -104,8 +146,7 @@ class gaussian_kde(st.gaussian_kde):
         BCinv = B @ np.linalg.inv(C)
         cov = A - BCinv @ B.T
         mus = np.swapaxes(self.dataset[dims_samp] +
-                          BCinv @ (x_cond[:,:,None] - self.dataset[dims_cond]),
-                          1, 2)
+                          BCinv @ (x_cond[:,:,None] - self.dataset[dims_cond]), 1, 2)
 
         # Sample from conditional kernel pdfs
         # Repeat means as many times as they were sampled in counts