005364b57c8c00e732857402c6487e199bf5f50b
Mutahar Chalmers <mut.chalmers@gmail.com>
2024-07-23 11:39:05 +0100

    Updated CV bandwidth selection and types

--- kdetools/kdecond.py
+++ kdetools/kdecond.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import scipy.linalg as sl
+import scipy.optimize as so
 import scipy.stats as st
 from scipy._lib._util import check_random_state
 
@@ -64,36 +66,112 @@ class gaussian_kde(st.gaussian_kde):
         return [(np.concatenate([splits[i] for i in set(range(k))-{j}]),
                  splits[j]) for j in range(k)]
 
-    def set_bandwidth(self, bw_method=None, k=5, delta=1, n=101):
+    def silverman_factor_ref(self):
+        """Compute the (refined) Silverman factor.
+
+        Returns
+        -------
+        s : ndarray
+            The Silverman factor(s).
+        """
+        iqrs = np.diff(np.quantile(self.dataset, [0.25, 0.75], axis=1), axis=0)
+        return (0.9*np.minimum(iqrs/1.34, self.dataset.std(ddof=1, axis=1))
+                *np.power(self.neff, -1/5)
+               ).ravel()/self.dataset.std(ddof=1, axis=1)
+
+    def set_bandwidth(self, bw_method=None, bw_type='diagonal', k=None):
         """Add bandwidth selection by cross-validation.
 
         Parameters
         ----------
         bw_method : str, scalar or callable, optional
             As parent class, with extra 'cv' option.
+        bw_type : str, optional
+            Type of bandwidth matrix. Options are `diagonal`,
+            `covariance` and `equal`.
         k : int, optional
-            Number of folds in cross-validation.
-        delta : float, optional
-            Defines range of bandwidth grid in log space.
-        n : int, optional
-            Defines number of points in bandwidth grid.
+            Number of folds in cross-validation. Leave One Out by default.
         """
 
         if bw_method == 'cv':
-            # Define trial bandwidths around initial guess of Silverman factor
-            factor = self.silverman_factor()
-            bws = np.exp(np.log(factor) + np.linspace(-delta, delta, n))
+            # Define bandwidth log-likelihood functions for cross-validation
+            # Vector of factors scaling each dimension with no cross-covariance
+            if bw_type == 'diagonal':
+                h0 = self.dataset.std(ddof=1, axis=1) * self.silverman_factor_ref()
+                def negloglike(h, Xeval, Xfit):
+                    return -np.log(self._mvn_pdf(Xeval, Xfit, np.diag(h**2)
+                                                ).mean(axis=1)).sum()
+            # Single factor scaling data covariance matrix
+            elif bw_type == 'covariance':
+                h0 = self.silverman_factor_ref().mean()
+                def negloglike(h, Xeval, Xfit):
+                    return -np.log(self._mvn_pdf(Xeval, Xfit, np.cov(Xfit.T)*h**2
+                                                ).mean(axis=1)).sum()
+            # Single factor scaling in all dimensions with no cross-covariance
+            elif bw_type == 'equal':
+                h0 = self.dataset.std(ddof=1) * self.silverman_factor_ref().mean()
+                def negloglike(h, Xeval, Xfit):
+                    return -np.log(self._mvn_pdf(Xeval, Xfit, np.eye(self.d)*h**2
+                                                ).mean(axis=1)).sum()
+            else:
+                print('bw_type must be diagonal, covariance or equal')
+                return None
+            self.bw_type = bw_type
 
-            # Cross-validation on log-likelihoods
-            eps = np.spacing(1)
-            x = self.dataset.T
-            bw_lls = [np.mean([-np.log(self._mvn_pdf(x[j], x[i], bw**2*np.cov(x[i].T)
-                                                    ).mean(axis=1)+eps).sum()
-                               for i, j in self.kfold_split(x, k)])
-                      for bw in bws]
-            super(gaussian_kde, self).set_bandwidth(bw_method=bws[np.argmin(bw_lls)])
+            # Define cross-validation - default LOOCV
+            if k is None:
+                k = self.dataset[0].size
+            splits = self.kfold_split(self.dataset.T, k)
+
+            # Minimise negative log-likelihood CV
+            def negloglike_cv(h, X, splits):
+                return np.mean([negloglike(h, X[j], X[i]) for i, j in splits])
+            res = so.minimize(negloglike_cv, h0, args=(self.dataset.T, splits),
+                              method='nelder-mead')
+            self.h = res['x']
+            self.loglike_cv = -res['fun']
+            self.covariance_factor = lambda: self.h
+            self._compute_covariance()
         else:
+            self.bw_type = 'covariance_original'
             super(gaussian_kde, self).set_bandwidth(bw_method=bw_method)
+
+    def _compute_covariance(self):
+        """Computes the covariance matrix for each Gaussian kernel using
+        covariance_factor(). Custom version for bw_method='cv'.
+        """
+
+        # With bw_method='cv', factor can be a vector of values
+        self.factor = self.covariance_factor()
+
+        # Cache covariance and Cholesky decomp of covariance
+        if not hasattr(self, '_data_cho_cov'):
+            self._data_covariance = np.atleast_2d(np.cov(self.dataset, rowvar=1,
+                                                  bias=False, aweights=self.weights))
+            self._data_cho_cov = sl.cholesky(self._data_covariance, lower=True)
+
+        if self.bw_type == 'diagonal':
+            self.covariance = np.diag(self.factor**2)
+        elif self.bw_type == 'equal':
+            self.covariance = np.eye(self.d)*self.factor**2
+        else: # 'covariance' or 'covariance_original'
+            self.covariance = self._data_covariance * self.factor**2
+
+        self.cho_cov = sl.cholesky(self.covariance, lower=True).astype(np.float64)
+        self.log_det = 2*np.log(np.diag(self.cho_cov * np.sqrt(2*np.pi))).sum()
+
+    @property
+    def inv_cov(self):
+        """Custom for bw_method='cv' to handle possibly vector factor.
+        Just don't change the dataset attribute, as the CV-derived
+        bandwidths will not update on ruuning covariance_factor."""
+        self.factor = self.covariance_factor()
+        if self.bw_type == 'diagonal':
+            return np.linalg.inv(np.diag(self.factor**2))
+        elif self.bw_type == 'equal':
+            return np.linalg.inv(np.eye(self.d)*self.factor**2)
+        else: # 'covariance' or 'covariance_original'
+            return np.linalg.inv(self._data_covariance * self.factor**2)
 
     def conditional_resample(self, size, x_cond, dims_cond, seed=None):
         """Fast conditional sampling of estimated pdf.
